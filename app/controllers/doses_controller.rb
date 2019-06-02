0de0f2f163d6413c8036de4de19b033e70f3acfd
Paulo Rivera <josepauloovallerivera@gmail.com>
2019-06-02 14:25:41 +0900

    Updated all the pages, with new color. - Now you can delete doses. - On the cocktail show page: - Now the link to add a new dose is in the top along with the name. - Now the photo of the cocktail is sticky and will scroll along with the description and ingredientes bar. - The logo in the nav-bar has a new size. - The new Logo is now in the assets folder. - In the cocktail show, is a new thrash icon.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,13 @@
 class DosesController < ApplicationController
- def new
+  before_action :set_cocktail, only: [:new, :create, :destroy]
+
+  def new
     @dose = Dose.new
-    @ingredients = Ingredient.all
-    @cocktail = Cocktail.find(params[:cocktail_id])
+      @ingredients = Ingredient.all
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail), notice: 'Cocktail added'
@@ -17,7 +17,7 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@cocktail)
   end
@@ -26,5 +26,9 @@ class DosesController < ApplicationController
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :new, :create, :destroy ]
   end
-  resources :doses, only: [:delete]
 end
